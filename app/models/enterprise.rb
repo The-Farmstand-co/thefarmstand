f206e513b0aaade9d88dd5f8172bb557af83a2b8
Uriel Baez <iubaez@iCloud.com>
2020-09-29 17:36:58 -0500

    Update enterprise.rb

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -205,7 +205,7 @@ class Enterprise < ActiveRecord::Base
 
   def self.search(search)
     if search
-      enterprise_name = Enterprise.find_by(name: search)
+      enterprise_name = Enterprise.find_by_name(search)
       # enterprise_name = Enterprise.where("name LIKE :search", search: "%#{search}%")
       if enterprise_name
         self.where(enterprise_id: enterprise_name)
